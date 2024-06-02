1fb9cd47c0d7b0c67cefa1e72bf619d7f4fb1bc2
Javier <javier.rojas.blum@gmail.com>
2024-06-02 20:32:09 +0100

    Get Spring MVC - Spring 3.3.0

--- src/test/java/guru/springframework/msscbrewery/MsscBreweryApplicationTests.java
+++ src/test/java/guru/springframework/msscbrewery/MsscBreweryApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework.mssc_brewery;
+package guru.springframework.msscbrewery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
